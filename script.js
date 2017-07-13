d47c18460a7bbf8bbb0b9b0917d7bd068fec51aa
reda <redaaa99@hotmail.fr>
2017-07-13 16:48:06 +0100

    test

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-
-
 var on=false;
 $(".slider").click(function(){
   if(on)
   {
     on=false;
+    shutDevice();
   }
   else
   {
     on=true;
+    startDevice();
   }
 });
+
+function startDevice(){
+  $("#realCounter").innerHTML="_ _";
+  $("#realCounter").animate()
+}
+
+
+
+
+
+
