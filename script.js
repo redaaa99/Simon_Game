8e646af0cd8a024633d2d7a1890b4fab608707a0
redaaa <redaaa99@hotmail.fr>
2017-07-14 01:58:25 +0100

    now showing game memory

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 var on=false;
 var strict_on=false;
+var playTime=false;
 
-var game_stack=[];
-var game_memory=[];
+var player_stack=[];
+var game_memory=[1,2,3,4,1,2,3,3,2,4];
 var counter=0;
 
 
@@ -12,14 +13,16 @@ $(".slider").click(function(){
   {
     on=false;
     shutDevice();
-    game_stack=[];
+    player_stack=[];
+    game_memory=[];
 
   }
   else
   {
     on=true;
     startDevice();
-    game_stack=[];
+    player_stack=[];
+    game_memory=[];
   }
 });
 
@@ -30,6 +33,8 @@ function startDevice(){
     $('p')[1].innerHTML="0 0";
   });
   setTimeout(startGame, 3000);
+  game_memory=[];
+  player_stack=[];
 }
 
 function shutDevice(){
@@ -39,6 +44,8 @@ function shutDevice(){
   },600,function(){
     $('p')[1].innerHTML="_ _";
   });
+  player_stack=[];
+  game_memory=[];
 }
 
 function strictClicked(){
@@ -57,17 +64,28 @@ function strictClicked(){
 
 function startGame(){
   color = randomColor();
-  game_stack.push(color);
+  game_memory.push(color);
+  showGameMemory();
+}
+
+function addCounter(num)
+{
   counter++;
-  showCounter();
-  showColor(color);
+  if(counter<10)
+  {
+    $('p')[1].innerHTML="0 "+counter;
+  }
+  else
+  {
+    $('p')[1].innerHTML=((counter/10)+" "+(counter%10));
+  }
 }
 
 function showColor(number)
 {
   if(number==4)
   {
-    $("#red").css("background-color","#f56767");
+    $("#red").css("background-color","#ff2d2d");
   }
   if(number==3)
   {
@@ -75,21 +93,41 @@ function showColor(number)
   }
   if(number==2)
   {
-    $("#blue").css("background-color","#57a0fd");
+    $("#blue").css("background-color","#3030ff");
   }
   if(number==1)
   {
     $("#yellow").css("background-color","rgb(241, 255, 54)");
   }
   setTimeout(function(){
-    $("#red").css("background-color","#d63838");
+    $("#red").css("background-color","#841b1b");
     $("#green").css("background-color","#277b27");
-    $("#blue").css("background-color","#28288e");
-    $("#yellow").css("background-color","#c5c521");
-  },1200);
+    $("#blue").css("background-color","#020261");
+    $("#yellow").css("background-color","#8e8e10");
+  },300);
 }
 
 
 function randomColor(){
   return Math.floor((Math.random() * 4) + 1);
 }
+
+
+k=0;
+function showGameMemory()
+{
+  if(k<game_memory.length)
+  {
+    
+    showColor(game_memory[k]);
+    setTimeout(showGameMemory,1200);
+    addCounter(counter);
+    setTimeout(function(){k++;},100);
+  }
+  if(k==game_memory)
+  {
+    clearTimeout(t);
+  }
+  
+}
+
